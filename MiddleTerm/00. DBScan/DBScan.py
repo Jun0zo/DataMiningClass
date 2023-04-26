2889aec29082093921b5687946ed2365b3cde1f4
Jun0zo <joon0zo1022@gmail.com>
2023-04-26 14:28:51 +0900

    dbscan 주석추가

--- MiddleTerm/00. DBScan/DBScan.py
+++ MiddleTerm/00. DBScan/DBScan.py
@@ -3,33 +3,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-class DBScan:
-    def __init__(self, data=[]):
+def getL2(vector1, vector2):
+    distance = 0
+    x1, y1 = vector1
+    x2, y2 = vector2
+    distance += (x1-x2)**2 + (y1-y2)**2
+    return math.sqrt(distance)
+
+class DBSCAN:
+    '''
+        DBSCAN Class
+        
+        Attributes:
+            data (list) : 데이터 리스트
+            eps (float) : 클러스터를 위한 최소 이웃 거리
+            min_pts (int) : 클러스터를 위한 최소 점의 개수
+    '''
+    def __init__(self, data: list =[], eps:float =2.5, min_pts:int =3):
         self.data = data
         self.labels = [-1] * len(data)
-        self.eps = 2.5
-        self.min_pts = 3
-        
+        self.eps = eps
+        self.min_pts = min_pts
         self.cluster_id = 0
         
-    def getL2(self, i, vector1, vector2):
-        distance = 0
-        x1, y1 = vector1
-        x2, y2 = vector2
-        distance += (x1-x2)**2 + (y1-y2)**2
-        return math.sqrt(distance)
-        
-    def get_neighbors(self, point1):
-        return [point2_idx for point2_idx, point2 in enumerate(self.data) if self.getL2(point2_idx, point1, point2) < self.eps]
-
-    def is_center(self, point1):
-        return True if len(self.get_neighbors(point1)) > self.min_pts else False
+    def _get_neighbors(self, point: list) -> list:
+        '''
+         point과 이웃하는(eps거리보다 짧은) point를 list형태로 return하는 함수
+            Args:
+                input (list) : [x,y]형태로 주어짐
+            Returns:
+                output (list) : [[x1,y1], [x2,y2]...] 형태로 이웃하는 point들을 return
+        '''
+        return [point2_idx for point2_idx, point2 in enumerate(self.data) if getL2(point, point2) < self.eps]
     
-    def expand_cluster(self, idx, neighbor_idxs):
+    def _expand_cluster(self, neighbor_idxs) -> None:
+        '''
+        이웃하는 point들을 추가적으로 검사하는 함수
+            Args:
+                point (list) : [idx1, idx2 ...]형태로 주어짐
+            Returns:
+                output (None)
+        '''
+        # 모든 점들을 순회
         for neighbor_idx in neighbor_idxs:
+            # 한번도 방문한 적이 없으면
             if self.labels[neighbor_idx] == -1:
                 self.labels[neighbor_idx] = self.cluster_id
-                new_neighbor_idxs = self.get_neighbors(self.data[neighbor_idx])
+                new_neighbor_idxs = self._get_neighbors(self.data[neighbor_idx])
+                # 한번도 방문한 적이 없고, CorePoint조건을 만족하면 클러스터에 추가
                 if len(new_neighbor_idxs) > self.min_pts:
                     neighbor_idxs.extend(new_neighbor_idxs)
             elif self.labels[neighbor_idx] != self.cluster_id:
@@ -37,16 +58,25 @@ class DBScan:
                 break
    
     def clustering(self):
+        '''
+        DBSCAN방법으로 클러스터링하는 함수
+        '''
+        # 모든 점들을 순회
         for point_idx, point in enumerate(self.data):
+            # 이미 방문한 점은 pass
             if self.labels[point_idx] != -1:
                 continue
-            neighbor_idxs = self.get_neighbors(point)
+            # 이웃한 점들을 검색
+            neighbor_idxs = self._get_neighbors(point)
+            # CorePoint조건을 만족하지 못하면 보류
             if len(neighbor_idxs) < self.min_pts:
                 self.labels[point_idx] = -1
                 continue
+            # CorePoint조건을 만족하면 클러스터에 추가
             self.cluster_id += 1
             self.labels[point_idx] = self.cluster_id
-            self.expand_cluster(point_idx, neighbor_idxs)
+            # CorePoint의 이웃점들을 추가로 순회
+            self._expand_cluster(neighbor_idxs)
         print(self.labels)
         
     def plotting(self):
--- MiddleTerm/00. DBScan/main.py
+++ MiddleTerm/00. DBScan/main.py
@@ -1,16 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from DBScan import DBScan
+from DBSCAN import DBSCAN
 
 csv = pd.read_csv("data.csv", sep="\t")
 data = csv.iloc[:,0:2].values.tolist()
 
-# X = [d[0] for d in data]
-# Y = [d[1] for d in data]
-# plt.scatter(X, Y)
-# plt.show()
-
-dbscan = DBScan(data=data)
+dbscan = DBSCAN(data=data)
 
 dbscan.plotting()
 dbscan.clustering()
