0d9f4718b2b44c98ae8069f1c82696ed5953c5cb
Jun0zo <joon0zo1022@gmail.com>
2023-04-26 18:25:27 +0900

    DBSCAN 주석 추가

--- MiddleTerm/00. DBScan/main.py
+++ MiddleTerm/00. DBScan/main.py
@@ -1,11 +1,10 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from DBSCAN import DBSCAN
 
 csv = pd.read_csv("data.csv", sep="\t")
 data = csv.iloc[:,0:2].values.tolist()
 
-dbscan = DBSCAN(data=data)
+dbscan = DBSCAN(data=data, eps=0.15, min_pts=2)
 
 dbscan.plotting()
 dbscan.clustering()
